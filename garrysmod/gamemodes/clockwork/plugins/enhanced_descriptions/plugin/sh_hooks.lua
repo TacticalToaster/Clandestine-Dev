faa2bb4314e2e3186ad2106af2afe9f899048263
TacticalToaster <31429229+TacticalToaster@users.noreply.github.com>
2021-01-28 17:42:16 -0600

    Remove enhanced descriptions
    Got to make it look better and add config options

--- garrysmod/gamemodes/clockwork/plugins/enhanced_descriptions/plugin/sh_hooks.lua
+++
@@ -1,7 +0,0 @@
-local PLUGIN = PLUGIN;
-
-Clockwork.player:AddCharacterData("voiceDesc", NWTYPE_STRING, "");
-Clockwork.player:AddCharacterData("facialDesc", NWTYPE_STRING, "");
-Clockwork.player:AddCharacterData("featureDesc", NWTYPE_STRING, "");
-Clockwork.player:AddCharacterData("clothingDesc", NWTYPE_STRING, "");
-Clockwork.player:AddCharacterData("equipmentDesc", NWTYPE_STRING, "");
