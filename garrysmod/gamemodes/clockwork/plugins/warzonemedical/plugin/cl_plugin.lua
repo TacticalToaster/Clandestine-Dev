6b35c985c07f67f5a8fea2fcbe8f49630c290ea1
TacticalToaster <31429229+TacticalToaster@users.noreply.github.com>
2021-01-28 19:07:06 -0600

    fixed icons

--- garrysmod/gamemodes/clockwork/plugins/warzonemedical/plugin/cl_plugin.lua
+++ garrysmod/gamemodes/clockwork/plugins/warzonemedical/plugin/cl_plugin.lua
@@ -15,14 +15,14 @@ Clockwork.chatBox:RegisterClass("injury", "ic", function(info)
 
 		Clockwork.chatBox:SetMultiplier(.7);
 		
-		Clockwork.chatBox:Add(info.filtered, "icon16/injury.png", Color(204, 113, 88, 255), info.text);
+		Clockwork.chatBox:Add(info.filtered, "clandestine/icon16/injury.png", Color(204, 113, 88, 255), info.text);
 	end;
 end);
 
 Clockwork.chatBox:RegisterClass("injurykill", "ic", function(info)
 	//local localized = Clockwork.chatBox:LangToTable("ChatPlayerDisconnect", Color(200, 150, 200, 255), info.text);
 	
-	Clockwork.chatBox:Add(info.filtered, "icon16/death.png", Color(176, 97, 76, 255), info.text);
+	Clockwork.chatBox:Add(info.filtered, "clandestine/icon16/death.png", Color(176, 97, 76, 255), info.text);
 end);
 
 Clockwork.chatBox:RegisterClass("treat", "ic", function(info)
@@ -30,5 +30,5 @@ Clockwork.chatBox:RegisterClass("treat", "ic", function(info)
 	
 	Clockwork.chatBox:SetMultiplier(.7);
 
-	Clockwork.chatBox:Add(info.filtered, "icon16/treat.png", Color(150, 204, 41, 255), info.text);
+	Clockwork.chatBox:Add(info.filtered, "clandestine/icon16/treat.png", Color(150, 204, 41, 255), info.text);
 end);
